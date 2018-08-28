b7e3d627a317147ffdaeb05ebae0c818680e2738
SrikanteswaraRao Talluri <talluri@apache.org>
2018-08-28 23:31:16 +0530

    added comments to the java file

--- Sel.java
+++ Sel.java
@@ -5,12 +5,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Sel {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "/Users/talluri/Downloads/chromedriver");
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver = new ChromeDriver(); //open chrome browser
 
-        driver.get("http://www.google.com");
-        driver.findElement(By.name("q")).sendKeys("hello");
-        driver.findElement(By.name("q")).submit();
+        driver.get("http://www.google.com"); //open google.com
+        driver.findElement(By.name("q")).sendKeys("hello"); //type "hello"
+        driver.findElement(By.name("q")).submit(); //click search
 
-        driver.quit();
+        driver.quit(); //close browser
     }
 }
